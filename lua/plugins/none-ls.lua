a424dd86970a2e023bfd08f86705e3174eeff2cb
Salmonela7 <vingissimonas@gmail.com>
2024-03-17 11:02:30 +0200

    Add configuration

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.csharpier,
+            null_ls.builtins.formatting.prettier
+        })
+
+        vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+    end
+}
