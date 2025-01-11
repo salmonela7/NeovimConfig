c1b13c53ee9aa66dbf09a37a419f0a548425b992
salmonela7 <vingissimonas@gmail.com>
2025-01-11 14:42:43 +0200

    Plugin changes

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,14 +1,19 @@
 return {
-  {
-    "tpope/vim-fugitive",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
-  },
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+        end,
+    },
+    {
+        "sindrets/diffview.nvim",
+        config = function()
+        end
+    }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		require("nvim-treesitter.configs").setup({
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
             lazy = false,
-			ensure_installed = {
+            ensure_installed = {
                 "lua",
-				"go",
-			},
+                "go",
+            },
             indent = { enable = true },
 
-			highlight = {
+            highlight = {
                 enable = true,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 kb
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-		})
-	end,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 kb
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+            },
+        })
+    end,
 }
