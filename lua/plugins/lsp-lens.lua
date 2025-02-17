5922e762f1eb151644e73b13920b620d9bf6e321
Simonas Vingis <SIMVI@danskebank.lt>
2025-02-17 14:34:44 +0200

    Improve lsplens

--- lua/plugins/lsp-lens.lua
+++ lua/plugins/lsp-lens.lua
@@ -2,7 +2,27 @@ return {
 	{
 		"VidocqH/lsp-lens.nvim",
 		config = function()
-			require("lsp-lens").setup({})
+			local SymbolKind = vim.lsp.protocol.SymbolKind
+
+			require("lsp-lens").setup({
+				enable = true,
+				sections = {
+					git_authors = false,
+					definition = false,
+					implements = false,
+					references = function(count)
+						return count .. " usages"
+					end,
+				},
+
+				target_symbol_kinds = {
+					SymbolKind.Function,
+					SymbolKind.Method,
+					SymbolKind.Interface,
+					SymbolKind.Class,
+					SymbolKind.Struct,
+				},
+			})
 		end,
 	},
 }
