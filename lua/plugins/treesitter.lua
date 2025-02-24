07d4a3a08fded7c9fe176ca01ddbbb9a7c6ba6ec
Simonas Vingis <SIMVI@danskebank.lt>
2025-02-24 08:53:43 +0200

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,17 +12,9 @@ return {
 			},
 			indent = { enable = true },
 
-			-- highlight = {
-			--     enable = true,
-			--     additional_vim_regex_highlighting = { "http" },
-			--     disable = function(_, buf)
-			--         local max_filesize = 100 * 1024 -- 100 kb
-			--         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			--         if ok and stats and stats.size > max_filesize then
-			--             return true
-			--         end
-			--     end,
-			-- },
+			highlight = {
+				enable = true,
+			},
 		})
 	end,
 }
