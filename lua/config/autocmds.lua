882535c98a77c673a30e41cd185016a2dc87ac59
Simonas Vingis <SIMVI@danskebank.lt>
2025-03-05 13:58:19 +0200

    telescope improvements

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,141 +1,141 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 vim.api.nvim_create_autocmd({ "FocusLost" }, {
-    command = "silent! wa",
+	command = "silent! wa",
 })
 
 vim.api.nvim_create_autocmd({ "BufLeave" }, {
-    command = "silent! wa",
+	command = "silent! wa",
 })
 
 vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
-    command = "silent! wa",
+	command = "silent! wa",
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 local augroup = vim.api.nvim_create_augroup
 local SalmonelaGroup = augroup("Salmonela", {})
 
 local function goToDefinitionAndCenterHandler(err, result, ctx, config)
-    local handler = require("vim.lsp.handlers")["textDocument/definition"]
+	local handler = require("vim.lsp.handlers")["textDocument/definition"]
 
-    if err ~= nil then
-        print("Error occurred on " .. ctx.method .. "...")
-        return
-    end
+	if err ~= nil then
+		print("Error occurred on " .. ctx.method .. "...")
+		return
+	end
 
-    if result == nil then
-        print(ctx.method .. " yielded no results...")
-        return
-    end
+	if result == nil then
+		print(ctx.method .. " yielded no results...")
+		return
+	end
 
-    local res, error = handler(err, result, ctx, config)
+	local res, error = handler(err, result, ctx, config)
 
-    local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
-    vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
+	local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
+	vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
 
-    return res, error
+	return res, error
 end
 
 local function goToReferencesAndCenterHandler(err, result, ctx, config)
-    local handler = require("vim.lsp.handlers")["textDocument/references"]
-    if err ~= nil then
-        print("Error occurred on " .. ctx.method .. "...")
-        return
-    end
+	local handler = require("telescope.builtin").lsp_references
+	if err ~= nil then
+		print("Error occurred on " .. ctx.method .. "...")
+		return
+	end
 
-    if result == nil then
-        print(ctx.method .. " yielded no results...")
-        return
-    end
+	if result == nil then
+		print(ctx.method .. " yielded no results...")
+		return
+	end
 
-    local res, error = handler(err, result, ctx, config)
+	local res, error = handler(err, result, ctx, config)
 
-    if #result == 1 then
-        local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
-        vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
-    end
+	if #result == 1 then
+		local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
+		vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
+	end
 
-    return res, error
+	return res, error
 end
 
 local function goToImplementationsAndCenterHandler(err, result, ctx, config)
-    local handler = require("vim.lsp.handlers")["textDocument/implementation"]
-    if err ~= nil then
-        print("Error occurred on " .. ctx.method .. "...")
-        return
-    end
+	local handler = require("telescope.builtin").lsp_implementations
+	if err ~= nil then
+		print("Error occurred on " .. ctx.method .. "...")
+		return
+	end
 
-    if result == nil then
-        print(ctx.method .. " yielded no results...")
-        return
-    end
+	if result == nil then
+		print(ctx.method .. " yielded no results...")
+		return
+	end
 
-    local res, error = handler(err, result, ctx, config)
+	local res, error = handler(err, result, ctx, config)
 
-    if #result == 1 then
-        local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
-        vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
-    end
+	if #result == 1 then
+		local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("zz", true, true, true)
+		vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
+	end
 
-    return res, error
+	return res, error
 end
 
 autocmd("LspAttach", {
-    group = SalmonelaGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float, {})
+	group = SalmonelaGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float, {})
 
-        -- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>gd", function()
-            local util = require("vim.lsp.util")
-            local params = util.make_position_params()
+		-- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>gd", function()
+			local util = require("vim.lsp.util")
+			local params = util.make_position_params()
 
-            vim.lsp.buf_request(0, "textDocument/definition", params, goToDefinitionAndCenterHandler)
-        end, {})
+			vim.lsp.buf_request(0, "textDocument/definition", params, goToDefinitionAndCenterHandler)
+		end, {})
 
-        vim.keymap.set("n", "<leader>gu", function()
-            local util = require("vim.lsp.util")
-            local params = util.make_position_params()
-            params.context = { includeDeclaration = false }
+		vim.keymap.set("n", "<leader>gu", function()
+			local util = require("vim.lsp.util")
+			local params = util.make_position_params()
+			params.context = { includeDeclaration = false }
 
-            vim.lsp.buf_request(0, "textDocument/references", params, goToReferencesAndCenterHandler)
-        end, {})
+			vim.lsp.buf_request(0, "textDocument/references", params, goToReferencesAndCenterHandler)
+		end, {})
 
-        vim.keymap.set("n", "<leader>gi", function()
-            local util = require("vim.lsp.util")
-            local params = util.make_position_params()
+		vim.keymap.set("n", "<leader>gi", function()
+			local util = require("vim.lsp.util")
+			local params = util.make_position_params()
 
-            vim.lsp.buf_request(0, "textDocument/implementation", params, goToImplementationsAndCenterHandler)
-        end, {})
+			vim.lsp.buf_request(0, "textDocument/implementation", params, goToImplementationsAndCenterHandler)
+		end, {})
 
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
-        vim.keymap.set("i", "<C-h>", function()
-            vim.lsp.buf.signature_help()
-        end, opts)
-        vim.keymap.set("n", "[d", function()
-            vim.diagnostic.goto_next()
-        end, opts)
-        vim.keymap.set("n", "]d", function()
-            vim.diagnostic.goto_prev()
-        end, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
 
-        vim.keymap.set({ "n", "v", "i" }, "<C-LeftMouse>", function()
-            local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("<LeftMouse>", true, true, true)
-            vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
-            vim.schedule(function()
-                vim.lsp.buf.definition()
-            end)
-        end, {})
-    end,
+		vim.keymap.set({ "n", "v", "i" }, "<C-LeftMouse>", function()
+			local keymap_with_termcodes_replaced = vim.api.nvim_replace_termcodes("<LeftMouse>", true, true, true)
+			vim.api.nvim_feedkeys(keymap_with_termcodes_replaced, "a", true)
+			vim.schedule(function()
+				vim.lsp.buf.definition()
+			end)
+		end, {})
+	end,
 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,111 +1,139 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
 
-            {
-                "nvim-lua/plenary.nvim",
-            },
-            {
-                "nvim-telescope/telescope-ui-select.nvim",
-            },
-            {
-                "nvim-telescope/telescope-live-grep-args.nvim",
-                version = "^1.0.0",
-            },
-            {
-                "gbrlsnchs/telescope-lsp-handlers.nvim",
-                config = function() end,
-            },
-        },
-        config = function()
-            local actions = require("telescope.actions")
-            local telescope = require("telescope")
-            local builtin = require("telescope.builtin")
+			{
+				"nvim-lua/plenary.nvim",
+			},
+			{
+				"nvim-telescope/telescope-ui-select.nvim",
+			},
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				version = "^1.0.0",
+			},
+			{
+				"gbrlsnchs/telescope-lsp-handlers.nvim",
+				config = function() end,
+			},
+		},
+		config = function()
+			local actions = require("telescope.actions")
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-            local function getVisualSelection()
-                vim.cmd('noau normal! "vy"')
-                local text = vim.fn.getreg("v")
-                vim.fn.setreg("v", {})
+			local function getVisualSelection()
+				vim.cmd('noau normal! "vy"')
+				local text = vim.fn.getreg("v")
+				vim.fn.setreg("v", {})
 
-                text = string.gsub(text, "\n", "")
-                if #text > 0 then
-                    return text
-                else
-                    return ""
-                end
-            end
+				text = string.gsub(text, "\n", "")
+				if #text > 0 then
+					return text
+				else
+					return ""
+				end
+			end
 
-            local telescope_ignore_patterns = {
-                "%_test.go",
-            }
+			local telescope_ignore_patterns = {
+				"%_test.go",
+			}
 
-            telescope.setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = actions.move_selection_previous,
-                            ["<C-j>"] = actions.move_selection_next,
-                            ["<leader>s"] = actions.file_vsplit + actions.center,
-                            ["<leader>S"] = actions.file_split + actions.center,
-                            ["<esc>"] = actions.close,
-                            ["<C-p>"] = actions.close,
-                            ["<CR>"] = actions.select_default + actions.center,
-                        },
-                        n = {
-                            ["<C-k>"] = actions.move_selection_previous,
-                            ["<C-j>"] = actions.move_selection_next,
-                            ["<leader>s"] = actions.file_vsplit + actions.center,
-                            ["<leader>S"] = actions.file_split + actions.center,
-                            ["<esc>"] = actions.close,
-                            ["<C-p>"] = actions.close,
-                            ["<CR>"] = actions.select_default + actions.center,
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+							["<leader>s"] = actions.file_vsplit + actions.center,
+							["<leader>S"] = actions.file_split + actions.center,
+							["<esc>"] = actions.close,
+							["<C-p>"] = actions.close,
+							["<CR>"] = actions.select_default + actions.center,
+						},
+						n = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+							["<leader>s"] = actions.file_vsplit + actions.center,
+							["<leader>S"] = actions.file_split + actions.center,
+							["<esc>"] = actions.close,
+							["<C-p>"] = actions.close,
+							["<CR>"] = actions.select_default + actions.center,
+						},
+					},
+				},
+				pickers = {
+					lsp_references = {
+						show_line = false,
+						include_declaration = false,
+						initial_mode = "normal",
+						on_complete = {
+							function(picker)
+								picker:clear_completion_callbacks()
+								if picker.manager.linked_states.size == 1 then
+									require("telescope.actions").select_default(picker.prompt_bufnr)
+								end
+							end,
+						},
+					},
+					lsp_implementations = {
+						show_line = false,
+						include_declaration = false,
+						initial_mode = "normal",
+						on_complete = {
+							function(picker)
+								picker:clear_completion_callbacks()
+								if picker.manager.linked_states.size == 1 then
+									require("telescope.actions").select_default(picker.prompt_bufnr)
+								end
+							end,
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-            vim.keymap.set("n", "<leader>ti", function()
-                vim.g.telescope_ignore_enabled = not vim.g.telescope_ignore_enabled
+			vim.keymap.set("n", "<leader>ti", function()
+				vim.g.telescope_ignore_enabled = not vim.g.telescope_ignore_enabled
 
-                require("telescope.config").set_defaults({
-                    file_ignore_patterns = vim.g.telescope_ignore_enabled and telescope_ignore_patterns or {},
-                })
-            end, { noremap = true, desc = "Toggle telescope ignore patterns" })
+				require("telescope.config").set_defaults({
+					file_ignore_patterns = vim.g.telescope_ignore_enabled and telescope_ignore_patterns or {},
+				})
+			end, { noremap = true, desc = "Toggle telescope ignore patterns" })
 
-            vim.keymap.set(
-                "n",
-                "<C-p>",
-                "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--no-ignore-vcs' }})<cr>",
-                {}
-            )
-            vim.keymap.set(
-                "v",
-                "<C-p>",
-                "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--no-ignore-vcs' }})<cr>",
-                {}
-            )
+			vim.keymap.set(
+				"n",
+				"<C-p>",
+				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--no-ignore-vcs' }})<cr>",
+				{}
+			)
+			vim.keymap.set(
+				"v",
+				"<C-p>",
+				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '--no-ignore-vcs' }})<cr>",
+				{}
+			)
 
-            vim.keymap.set("v", "<leader>ff", function()
-                local text = getVisualSelection()
-                telescope.extensions.live_grep_args.live_grep_args({ default_text = text })
-            end, {})
-            vim.keymap.set("n", "<leader>ff", function()
-                telescope.extensions.live_grep_args.live_grep_args()
-            end, {})
-            vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-            vim.keymap.set("n", "<leader>fs", builtin.lsp_dynamic_workspace_symbols, {})
+			vim.keymap.set("v", "<leader>ff", function()
+				local text = getVisualSelection()
+				telescope.extensions.live_grep_args.live_grep_args({ default_text = text })
+			end, {})
+			vim.keymap.set("n", "<leader>ff", function()
+				telescope.extensions.live_grep_args.live_grep_args()
+			end, {})
+			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>fs", builtin.lsp_dynamic_workspace_symbols, {})
 
-            telescope.load_extension("ui-select")
-            telescope.load_extension("live_grep_args")
-            telescope.load_extension("lsp_handlers")
-        end,
-    },
+			telescope.load_extension("ui-select")
+			telescope.load_extension("live_grep_args")
+			telescope.load_extension("lsp_handlers")
+		end,
+	},
 }
