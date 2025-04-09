67a503e27526155babb24dd070282915a968a69c
Simonas Vingis <SIMVI@danskebank.lt>
2025-04-09 13:41:27 +0300

    improvements

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -2,3 +2,8 @@ vim.keymap.set("n", "<leader>at", function()
 	vim.cmd("silent! wa")
 	vim.cmd("Floatexecute dotnet test")
 end)
+
+vim.keymap.set("n", "<F4>", function()
+	vim.cmd("silent! wa")
+	vim.cmd("Floatexecute dotnet build")
+end)
