00b2dc57d4ffbd2c9e15ad546bf4c866c43041de
Simonas Vingis <SIMVI@danskebank.lt>
2025-04-18 16:44:07 +0300

    add copilot :(

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"github/copilot.vim",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, 
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		opts = {},
+        config = function()
+            require("CopilotChat").setup({
+                vim.keymap.set("n", "<leader>co", function()
+                    require("CopilotChat").toggle()                
+                end, { desc = "Open Copilot Chat" }),
+            })
+        end,
+	},
+}
