c01d473ffcc91355c6025cf6fd1d6de75e23f3cb
Simonas Vingis <vingissimonas@gmail.com>
2025-04-18 21:27:48 +0300

    fix copilot chate

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,16 +5,22 @@ return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{ "github/copilot.vim" }, 
+			{ "github/copilot.vim" },
 			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
 		opts = {},
-        config = function()
-            require("CopilotChat").setup({
-                vim.keymap.set("n", "<leader>co", function()
-                    require("CopilotChat").toggle()                
-                end, { desc = "Open Copilot Chat" }),
-            })
-        end,
+		config = function()
+			require("CopilotChat").setup({
+				mappings = {
+					reset = {
+						normal = "<C-r>",
+						insert = "<C-r>",
+					},
+				},
+			})
+				vim.keymap.set("n", "<leader>co", function()
+					require("CopilotChat").toggle()
+				end, { desc = "Open Copilot Chat" })
+		end,
 	},
 }
