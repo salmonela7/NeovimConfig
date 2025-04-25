a08e60e2a174bbad9b84b55d3afb4ec955889fbc
Simonas Vingis <SIMVI@danskebank.lt>
2025-04-25 10:28:36 +0300

    change colorscheme to moonfly

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,7 +23,7 @@ if utils.IS_WINDOWS then
 	vim.opt.shellxquote = ""
 end
 
-vim.cmd("colorscheme gruvbox-material")
+vim.cmd("colorscheme moonfly")
 
 vim.cmd([[highlight DiagnosticVirtualTextError guifg=#ea6962]])
 vim.cmd([[highlight DiagnosticVirtualTextWarn guifg=#d8a657]])
